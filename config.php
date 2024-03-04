3a3260bf868684514b787db3d9b3802fe9f949f0
M. Davaee <mdavaee@gmail.com>
2024-03-04 14:22:15 +0330

    docs(config): update documentations

--- config.php
+++ config.php
@@ -2,8 +2,27 @@
 
 return [
 
-    'number_of_participants' => 2,
+    /*
+    |--------------------------------------------------------------------------
+    | Cars Dataset
+    |--------------------------------------------------------------------------
+    |
+    | This option determines where the vehicle dataset should be extracted from.
+    |
+    */
 
     'cars_dataset_path' => __DIR__.'/vehicles.json',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Game Participants
+    |--------------------------------------------------------------------------
+    |
+    | Here, you may specify the number of game participants.
+    | A game has at least two or more participants.
+    |
+    */
+
+    'number_of_participants' => 2,
 
 ];
